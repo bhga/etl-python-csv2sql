98a4d8c3c4fdcc3a55f19b0e2a5c1663f360c792
jeevanBhagade <41315882+bhga@users.noreply.github.com>
2022-12-10 20:29:44 +0530

    Added reject path

--- csv2mysql.py
+++ csv2mysql.py
@@ -2,33 +2,39 @@ import csv
 import MySQLdb
 from datetime import date
 
-
 inputpath=input("Enter the input path file:")
-
 outputpath=input("Enter the output path file:")
+rejectpath=input("Enter the reject path file:")
 
 dlm=input("Enter delimiter(must be a 1-character string):") #e.g ';','|',','
 
 with open(inputpath,'r') as file:
     csvreader = csv.DictReader(file,delimiter=dlm)
-    print("Succesfully read the input file!!!")
+    print("Successfully read the input file")
 
 
     with open(outputpath,'w',newline='') as outfile:
         columnname=['col1','col2','col3','col4','col5']
         csvwriter=csv.DictWriter(outfile,fieldnames=columnname,delimiter=dlm)
         csvwriter.writeheader()
-        for r in csvreader:
-            if any(field.strip() for field in r):
-                if (r["col1"]!=''):
-                    if (r["col2"]==''):
-                        r["col2"]="NA"
+        
+        with open(rejectpath,'w',newline='') as rejectfile:
+            csvrejectwriter=csv.DictWriter(rejectfile,fieldnames=columnname,delimiter=dlm)
+            csvrejectwriter.writeheader()
+            for r in csvreader:
+                if any(field.strip() for field in r):
+                    if (r["col1"]!=''):
+                        if (r["col2"]==''):
+                            r["col2"]="NA"
+                        else:
+                            r["col2"]=r["col2"]+r["col3"]
+                        r.update({"col5":date.today()})
+                        csvwriter.writerow(r)
                     else:
-                        r["col2"]=r["col2"]+r["col3"]
-                    r.update({"col5":date.today()})
-                    csvwriter.writerow(r)
-
-print("Succesfully created the output file!!!")
+                        r.update({"col5":date.today()})
+                        csvrejectwriter.writerow(r)
+                                
+print("Successfully created the output file!!!")
 db=MySQLdb.connect(host='',user='',password='',database='')
 
 with open(outputpath,'r') as generatedfile:
